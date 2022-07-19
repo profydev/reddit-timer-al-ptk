d45b3a72ff01c77fe0ad8941f3b3e55d369f53f4
Alan Patrick <alan.patrick.fv@gmail.com>
2022-07-19 14:56:44 -0300

    Add minimal styling to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 import logo from '../assets/images/logo.svg';
 
+const StyledHeader = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px 40px;
+`;
+
+const StyledNav = styled.nav`
+  a {
+    text-decoration: none;
+    color: black;
+    margin-left: 20px;
+  }
+`;
+
 const Header = () => (
-  <header>
+  <StyledHeader>
     <Link to="/">
       <img src={logo} alt="Reddit Timer Logo" />
     </Link>
-    <nav>
+    <StyledNav>
       <Link to="/search">Search</Link>
       <Link to="#How-it-works">How it works</Link>
       <Link to="#About">About</Link>
-    </nav>
-  </header>
+    </StyledNav>
+  </StyledHeader>
 );
 
 export default Header;
