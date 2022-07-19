cfc3a7e5208ebf955677219d144b4de0afd322a1
Alan Patrick <alan.patrick.fv@gmail.com>
2022-07-19 11:44:37 -0300

    Add Footer component

--- src/pages/Home/Home.page.js
+++ src/pages/Home/Home.page.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer.component';
+import Header from '../../components/Header/Header.component';
 
 const Home = () => (
   <div>
     <Header />
     Under Construction
+    <Footer />
   </div>
 );
 
