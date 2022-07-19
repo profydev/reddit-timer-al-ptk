002c526fda89ff7eeb6487c0a5cc650b374cf2b0
Alan Patrick <alan.patrick.fv@gmail.com>
2022-07-19 16:06:39 -0300

    Update App

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { Normalize } from 'styled-normalize';
 import Home from './pages/Home';
 import Search from './pages/Search';
+import GlobalStyle from './GlobalStyle';
+import theme from './theme';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/javascript" element={<Search />} />
-        <Route path="*" element={<div>404</div>} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Normalize />
+        <GlobalStyle />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/javascript" element={<Search />} />
+          <Route path="*" element={<div>404</div>} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
